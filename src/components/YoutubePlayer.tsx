a037e5a31d730b8025e6091ba6e7b80781420170
Demid <vasilyev.demid.e@gmail.com>
2022-12-21 13:16:32 +0300

    Fix youtube player reloading

--- src/components/YoutubePlayer.tsx
+++ src/components/YoutubePlayer.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from "react";
-import YouTube, { type YouTubeProps } from "react-youtube";
+import YouTube, { type YouTubeProps, type YouTubePlayer } from "react-youtube";
 import { useTracks } from "../contexts/Tracks";
 import type { ActiveTrack } from "../utils/types";
 
@@ -7,44 +7,61 @@ const YoutubePlayer = () => {
   const { activeTrack, setNextActiveTrack } = useTracks();
 
   const player = useRef<YouTube>(null);
+  const internalPlayer = useRef<YouTubePlayer | null | undefined>(null);
 
   const prevActiveTrack = useRef<ActiveTrack>(null);
-
-  useEffect(() => {
-    const internalPlayer = player.current?.getInternalPlayer();
-
-    if (!internalPlayer) {
-      return;
-    }
-
-    internalPlayer.getPlayerState().then((playerState: number) => {
-      if (
-        JSON.stringify(activeTrack) === JSON.stringify(prevActiveTrack.current)
-      ) {
-        if (playerState === YouTube.PlayerState.PLAYING) {
-          internalPlayer.pauseVideo();
-        } else if (playerState === YouTube.PlayerState.PAUSED) {
-          internalPlayer.playVideo();
-        }
-      } else {
-        prevActiveTrack.current = activeTrack;
-      }
-    });
-  }, [activeTrack]);
 
   const opts: YouTubeProps["opts"] = {
     height: "280",
     width: "500",
-    playerVars: {
-      autoplay: 1,
-    },
   };
+
+  const onPlayerReady: YouTubeProps["onReady"] = () => {
+    internalPlayer.current = player.current?.getInternalPlayer();
+  };
+
+  useEffect(() => {
+    if (!activeTrack) {
+      return;
+    }
+
+    internalPlayer.current
+      ?.getPlayerState()
+      .then((playerState) => {
+        if (!internalPlayer.current) {
+          return;
+        }
+
+        if (
+          JSON.stringify(activeTrack) ===
+          JSON.stringify(prevActiveTrack.current)
+        ) {
+          if (playerState === YouTube.PlayerState.PLAYING) {
+            internalPlayer.current.pauseVideo();
+          } else if (playerState === YouTube.PlayerState.PAUSED) {
+            internalPlayer.current.playVideo();
+          }
+        } else {
+          if (!internalPlayer.current) {
+            return;
+          }
+
+          internalPlayer.current
+            .loadVideoById(activeTrack.youtube_video_id)
+            .then(() => internalPlayer.current?.playVideo())
+            .catch((error) => console.error("Error in loadVideoById:", error));
+
+          prevActiveTrack.current = activeTrack;
+        }
+      })
+      .catch((error) => console.error("Error in getPlayerState:", error));
+  }, [activeTrack]);
 
   return (
     <YouTube
       ref={player}
-      videoId={activeTrack?.youtube_video_id}
       opts={opts}
+      onReady={onPlayerReady}
       onEnd={setNextActiveTrack}
       onError={setNextActiveTrack}
       className={
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -4,15 +4,14 @@ import YoutubePlayer from "../components/YoutubePlayer";
 import Theme from "../contexts/Theme";
 import Tracks from "../contexts/Tracks";
 
-const Main: FC<{ children: ReactNode }> = ({ children }) => (
+const MainLayout: FC<{ children: ReactNode }> = ({ children }) => (
   <Tracks>
     <Theme>
       <Header />
       <main className="pt-10">{children}</main>
     </Theme>
-
     <YoutubePlayer />
   </Tracks>
 );
 
-export default Main;
+export default MainLayout;
