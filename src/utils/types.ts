368da21239df67bc5c038d0e44c5cea5e469fd4f
Demid <vasilyev.demid.e@gmail.com>
2023-01-12 01:01:49 +0300

    Remove apis for dump-lush-db

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,9 +1,3 @@
 type ActiveTrack = { id: number; youtube_video_id: string } | null;
 
-type Table = { name: string };
-
-type TablePreview = Table & {
-  where?: Record<string, number>[];
-};
-
-export type { ActiveTrack, Table, TablePreview };
+export type { ActiveTrack };
