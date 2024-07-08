debe8da13daf291ccfa0dc4d3cd3b586ee59b256
Demid <vasilyev.demid.e@gmail.com>
2024-07-08 18:29:06 +0300

    Move pages to folders

--- src/pages/playlists/index.tsx
+++ src/pages/playlists/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import PlaylistsBlock from "../components/PlaylistsBlock";
+import PlaylistsBlock from "../../components/PlaylistsBlock";
 
 const Playlists: NextPage = () => (
   <>
