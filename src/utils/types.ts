e3d81a740ddb9201f843007f353ea33317dcc152
Demid <vasilyev.demid.e@gmail.com>
2024-07-09 23:04:52 +0300

    Fix types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,11 +2,11 @@ import type { RouterOutputs } from "./trpc";
 
 export type ActiveTrack = { id: number; youtube_video_id: string };
 
-export type TracksData = RouterOutputs["tracks"]["getTracks"]["tracks"];
+export type TracksData = RouterOutputs["track"]["page"]["tracks"];
 export type TrackData = TracksData[0];
 
 export type AttachedImage =
-  RouterOutputs["artists"]["getArtists"]["artists"][0]["artist_image_rels"][0]["artist_image"] & {
+  RouterOutputs["artist"]["page"]["artists"][0]["artist_image_rels"][0]["artist_image"] & {
     r?: number;
     g?: number;
     b?: number;
