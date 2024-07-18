ea812de1729d372c51ca515dfbd95f55323423c7
Demid <vasilyev.demid.e@gmail.com>
2024-07-18 17:25:42 +0300

    Fix fallback images

--- src/components/FaultTolerantImage.tsx
+++ src/components/FaultTolerantImage.tsx
@@ -15,16 +15,26 @@ const FaultTolerantImage: FC<{
     height?: number;
     w: string;
   };
-}> = ({ image }) => {
+}> = ({ image, defaultImage }) => {
   return (
     <>
-      {image.url && (
+      {image.url !== undefined ? (
         <Image
           src={image.url}
           alt={image.alt}
           width={image.width}
           height={image.height}
           className={"absolute h-full w-full object-cover"}
+        />
+      ) : (
+        <Image
+          src={defaultImage.url}
+          alt={defaultImage.alt ?? ""}
+          width={defaultImage.width ?? image.width}
+          height={defaultImage.width ?? image.width}
+          className={
+            "absolute bottom-0 left-0 right-0 top-0 m-auto " + defaultImage.w
+          }
         />
       )}
     </>
