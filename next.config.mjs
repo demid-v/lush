0578517befac6abdce94ed1fc473376883bb743f
Demid <vasilyev.demid.e@gmail.com>
2024-07-18 18:25:05 +0300

    Add t3 env validation

--- next.config.mjs
+++ next.config.mjs
@@ -3,16 +3,10 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
-!process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
+await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
-  swcMinify: true,
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
   images: {
     remotePatterns: [
       {
