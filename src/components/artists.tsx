11c03470dea6d6aad37efa07a21c4ac68e8e74d1
Demid <vasilyev.demid.e@gmail.com>
2024-07-25 20:23:44 +0300

    Add infinite scroll to artists page

--- src/components/artists.tsx
+++ src/components/artists.tsx
@@ -3,16 +3,23 @@
 import Tile from "./tile";
 import { useSearchParams } from "next/navigation";
 import { useMemo } from "react";
+import { useInView } from "react-intersection-observer";
 import { api } from "~/trpc/react";
 
 const defaultImage = "/assets/note.svg";
+const artistsLimit = 120;
 
 const Artists = () => {
   const searchParams = useSearchParams();
   const q = searchParams?.get("q")?.toString();
 
-  const { data: artistsData } = api.artist.page.useInfiniteQuery(
-    { search: q, limit: 120 },
+  const {
+    data: artistsData,
+    isFetching,
+    hasNextPage,
+    fetchNextPage,
+  } = api.artist.page.useInfiniteQuery(
+    { search: q, limit: artistsLimit },
     { getNextPageParam: (lastPage) => lastPage.nextCursor },
   );
 
@@ -21,11 +28,25 @@ const Artists = () => {
     [artistsData?.pages],
   );
 
+  const { ref: artistsRef } = useInView({
+    triggerOnce: true,
+    onChange(inView) {
+      if (!inView || isFetching || !hasNextPage) return;
+
+      void fetchNextPage();
+    },
+  });
+
+  const shouldObserve = (index: number) =>
+    artistsData?.pages.at(-1)?.artists.length === artistsLimit &&
+    index === artists.length - 1;
+
   return (
     <>
-      {artists.map(({ id, name, artist_image_rels: images }) => (
+      {artists.map(({ id, name, artist_image_rels: images }, index) => (
         <Tile
           key={id}
+          ref={shouldObserve(index) ? artistsRef : null}
           data={{
             id,
             domain: "artists",
--- src/components/tile.tsx
+++ src/components/tile.tsx
@@ -1,50 +1,63 @@
 import Link from "next/link";
-import type { FC } from "react";
 import type { AttachedImage } from "../utils/types";
 import { DOMAIN_MID_PATH } from "../utils/globals";
 import FaultTolerantImage from "./fault-tolerant-image";
+import type { ForwardedRef } from "react";
+import { forwardRef } from "react";
 
-const Tile: FC<{
-  data: {
-    id: number;
-    domain: "artists" | "albums" | "playlists";
-    name: string | null;
-    image: AttachedImage | undefined;
-    defaultImage: string;
-  };
-}> = ({ data: { id, domain, name, image, defaultImage } }) => {
-  const tileName = name || "";
-  const imageUrl =
-    image &&
-    image.domain.name + "/" + DOMAIN_MID_PATH[image.domain.id] + image.image_id;
+const Tile = forwardRef(
+  (
+    {
+      data: { id, domain, name, image, defaultImage },
+    }: {
+      data: {
+        id: number;
+        domain: "artists" | "albums" | "playlists";
+        name: string | null;
+        image: AttachedImage | undefined;
+        defaultImage: string;
+      };
+    },
+    ref: ForwardedRef<HTMLLIElement>,
+  ) => {
+    const tileName = name || "";
+    const imageUrl =
+      image &&
+      image.domain.name +
+        "/" +
+        DOMAIN_MID_PATH[image.domain.id] +
+        image.image_id;
 
-  return (
-    <li>
-      <div>
-        <Link
-          href={`/${domain}/${id}+${tileName}`}
-          aria-label={tileName}
-          className="mb-3 block"
-        >
-          <div className="relative pb-[100%]">
-            <FaultTolerantImage
-              image={{
-                url: imageUrl,
-                alt: "Image of " + name,
-                width: 256,
-                height: 256,
-              }}
-              defaultImage={{
-                url: defaultImage,
-                w: "w-[45%]",
-              }}
-            />
-          </div>
-        </Link>
-        <div className="font-medium">{name}</div>
-      </div>
-    </li>
-  );
-};
+    return (
+      <li ref={ref}>
+        <div>
+          <Link
+            href={`/${domain}/${id}+${tileName}`}
+            aria-label={tileName}
+            className="mb-3 block"
+          >
+            <div className="relative pb-[100%]">
+              <FaultTolerantImage
+                image={{
+                  url: imageUrl,
+                  alt: "Image of " + name,
+                  width: 256,
+                  height: 256,
+                }}
+                defaultImage={{
+                  url: defaultImage,
+                  w: "w-[45%]",
+                }}
+              />
+            </div>
+          </Link>
+          <div className="font-medium">{name}</div>
+        </div>
+      </li>
+    );
+  },
+);
+
+Tile.displayName = "Tile";
 
 export default Tile;
