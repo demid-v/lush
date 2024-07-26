417c2295b73644e465ff041daaa7816e985b3207
Demid <vasilyev.demid.e@gmail.com>
2024-07-26 15:12:37 +0300

    Add infinite scroll to albums page

--- src/components/albums.tsx
+++ src/components/albums.tsx
@@ -4,15 +4,22 @@ import { api } from "~/trpc/react";
 import Tile from "./tile";
 import { useMemo } from "react";
 import { useSearchParams } from "next/navigation";
+import { useInView } from "react-intersection-observer";
 
 const defaultImage = "/assets/vynil.svg";
+const albumsLimit = 120;
 
 const Albums = () => {
   const searchParams = useSearchParams();
   const q = searchParams?.get("q")?.toString();
 
-  const { data: albumsData } = api.album.page.useInfiniteQuery(
-    { search: q, limit: 120 },
+  const {
+    data: albumsData,
+    isFetching,
+    hasNextPage,
+    fetchNextPage,
+  } = api.album.page.useInfiniteQuery(
+    { search: q, limit: albumsLimit },
     { getNextPageParam: (lastPage) => lastPage.nextCursor },
   );
 
@@ -21,11 +28,25 @@ const Albums = () => {
     [albumsData?.pages],
   );
 
+  const { ref: albumsRef } = useInView({
+    triggerOnce: true,
+    onChange(inView) {
+      if (!inView || isFetching || !hasNextPage) return;
+
+      void fetchNextPage();
+    },
+  });
+
+  const shouldObserve = (index: number) =>
+    albumsData?.pages.at(-1)?.albums.length === albumsLimit &&
+    index === albums.length - 1;
+
   return (
     <>
-      {albums.map(({ id, name, album_image_rels: images }) => (
+      {albums.map(({ id, name, album_image_rels: images }, index) => (
         <Tile
           key={id}
+          ref={shouldObserve(index) ? albumsRef : null}
           data={{
             id,
             domain: "albums",
