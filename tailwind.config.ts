5f8e6ac5db52ad6e0758cb181148965b5561b591
Demid <vasilyev.demid.e@gmail.com>
2024-08-20 00:50:37 +0300

    Fix background color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,7 +23,7 @@ const config = {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
+        background: "rgb(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
           DEFAULT: "hsl(var(--primary))",
